13891e81d346047ad9c8b8793d48b6640f55028c
zahidi14 <d.zahidi1412@gmail.com>
2024-05-29 07:17:02 +0800

    fix resp

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "192.168.1.81",
-    port: 5173,
-  },
+  
   plugins: [
     react(),
     svgr({
